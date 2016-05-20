03fa9e197d50c4f6687595831526b700ab4abf89
Matthias Wölbitsch <m.woelbitsch@student.tugraz.at>
2016-05-20 16:35:10 +0200

    added preprocessing (standardization) when loading the data

--- src/utils.py
+++ src/utils.py
@@ -32,7 +32,9 @@ def read_dataset(path):
 def split_dataset(data, window, ratio=0.7):
     """
     Generates training and test data from the data set. Each sample consists of
-    a sequence of data points and a target forecast value.
+    a sequence of data points and a target forecast value. The values in the
+    data set are standardized to get data with zero mean and a standard
+    deviation of one.
 
     Args:
         data: The data set as pandas DataFrame.
@@ -45,18 +47,26 @@ def split_dataset(data, window, ratio=0.7):
         which should be used for training.
 
     Returns:
-        A 4-tuple consisting of:
+        A 6-tuple consisting of:
         1) A numpy array of training sequences.
         2) A numpy array of the corresponding target values for the training
            sequences.
         3) A numpy array of test sequences.
         4) A numpy array of the corresponding target values for the test
            sequences.
+        5) The mean of the original data.
+        6) The standard deviation of the original data.
     """
     if not 0.0 <= ratio <= 1:
         raise ValueError("invalid value for split ratio.")
 
     window.append(True) # include the target value in the sequences
+
+    # standardizing the data to get a zero mean and standard deviation of one
+    mean = data['Internet traffic data (in GB)'].mean()
+    std = data['Internet traffic data (in GB)'].std()
+    data['Internet traffic data (in GB)'] -= mean
+    data['Internet traffic data (in GB)'] /= std
 
     sequences = []
     for index in range(len(data) - len(window) + 1):
@@ -73,4 +83,14 @@ def split_dataset(data, window, ratio=0.7):
     X_test = test[:, :-1]
     y_test = test[:, -1]
 
-    return X_train, y_train, X_test, y_test
+    return X_train, y_train, X_test, y_test, mean, std
+
+def _print_test_data():
+    data = read_dataset('../datasets/internet-traffic-data-5minutes.csv')
+    X_test, y_test, _, _, mean, std = split_dataset(data, [True] * 50)
+    print("test data: ", X_test)
+    print("target values: ", y_test)
+    print("mean and std of the original data set: ", mean, "and ", std)
+
+if __name__ == '__main__':
+    _print_test_data()
