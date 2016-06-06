2ec1cca9ed379c9900787e8699fa5d45db9398c9
Matthias Wölbitsch <m.woelbitsch@student.tugraz.at>
2016-06-06 16:47:59 +0200

    added a hyper parameter search function

--- src/mlp.py
+++ src/mlp.py
@@ -9,6 +9,8 @@ from keras.callbacks import EarlyStopping
 from utils import read_dataset, split_dataset
 
 
+LOSS_FUNCTION = 'mae' #loss=mean_squared_logarithmic_error
+
 def store_model(model):
     model_id = time.strftime('%A_%H%M%S')
     with open('../models/model_{}.json'.format(model_id), 'x') as json_file:
@@ -19,7 +21,7 @@ def load_model(model_id):
     with open('../models/model_{}.json'.format(model_id)) as json_file:
         model = model_from_json(json_file.read())
     model.load_weights('../models/model_{}.h5'.format(model_id))
-    model.compile(optimizer='sgd', loss='mae') #loss=mean_squared_logarithmic_error
+    model.compile(optimizer='sgd', loss=LOSS_FUNCTION)
     return model
 
 def plot_result(forecast, actual, mean, std, export_path=None):
@@ -34,13 +36,13 @@ def plot_result(forecast, actual, mean, std, export_path=None):
     else:
         plt.savefig(export_path)
 
-def compile_model(hidden_neurons, input_dim):
+def compile_model(hidden_neurons, input_dim, activation_function='sigmoid'):
     model = Sequential()
     model.add(Dense(hidden_neurons, input_dim=input_dim))
-    model.add(Activation('sigmoid'))
+    model.add(Activation(activation_function))
     model.add(Dense(1))
     model.add(Activation('linear'))
-    model.compile(optimizer='sgd', loss='mae') #loss=mean_squared_logarithmic_error
+    model.compile(optimizer='sgd', loss=LOSS_FUNCTION)
     return model
 
 def run_network(show_plot=False):
@@ -70,6 +72,42 @@ def run_network(show_plot=False):
 
     print('\ntotoal duration: {:.2f} seconds'.format(time.time() - start_time))
     return model
+
+def hyper_parameter_search():
+    from hyperopt import fmin, tpe, hp, STATUS_OK, STATUS_FAIL
+
+    data = read_dataset('../datasets/internet-traffic-data-5minutes.csv')
+    space = {
+        'nneurons': hp.randint('nneurons', 41),
+        'window': hp.randint('window', 2048),
+        'season': hp.choice('season', ['no_season', 'half_day', 'full_day']),
+        'activation_function': hp.choice('func', ['sigmoid', 'tanh', 'relu'])
+    }
+
+    def objective(params):
+        nneurons = params['nneurons']
+        window = [int(digit) for digit in bin(params['window'])[2:]]
+
+        if params['season'] == 'full_day':
+            window += [0] * (289 - len(window) - 3) + [1, 1, 1]
+        if params['season'] == 'half_day':
+            window += [0] * (169 - len(window) - 3) + [1, 1, 1]
+
+        if not any(window) or nneurons < 2:
+            return {'status': STATUS_FAIL}
+
+        X_train, y_train, *_ = split_dataset(data, window, ratio=0.75)
+        model = compile_model(nneurons, input_dim=sum(1 for x in window if x),
+            activation_function=params['activation_function'])
+        hist = model.fit(X_train, y_train, nb_epoch=50, validation_split=0.33,
+            callbacks=[EarlyStopping(monitor='val_loss', patience=2)],
+            verbose=0)
+
+        val_loss = hist.history['val_loss'][-1]
+        print('model val_loss=', val_loss)
+        return {'loss': val_loss, 'status': STATUS_OK}
+
+    return fmin(fn=objective, space=space, algo=tpe.suggest, max_evals=2)
 
 if __name__ == '__main__':
     run_network(show_plot=True)
